805c98fd14cbc330dddaf826f686725a4bfff25a
Inessa Modina <modina.inessa@gmail.com>
2022-04-12 13:09:21 -0700

    Add validate functions

--- src/email-validator.js
+++ src/email-validator.js
@@ -2,11 +2,38 @@
 import * as constants from './constants.js';
 
 export function validate(email) {
-  const regExp = new RegExp(
-    constants.VALID_EMAIL_ENDINGS.map(element => `${element}$`)
+	const regExp = createRegExp(constants.VALID_EMAIL_ENDINGS);
+  return regExp.test(email);
+}
+
+export function validateAsync(email) {
+	const regExp = createRegExp(constants.VALID_EMAIL_ENDINGS);
+	let promise = new Promise(() => regExp.test(email));
+	return promise;
+}
+
+export function validateWithThrow(email) {
+	const regExp = createRegExp(constants.VALID_EMAIL_ENDINGS);
+
+	if (regExp.test(email)) {
+		return true;
+	} else {
+		throw new Error('Provided email is invalid!');
+	}
+}
+
+export function  validateWithLog(email) {
+	const regExp = createRegExp(constants.VALID_EMAIL_ENDINGS);
+	console.log(regExp.test(email));
+	return regExp.test(email);
+}
+
+function createRegExp (endingArray) {
+	const regExp = new RegExp(
+    endingArray.map(element => `${element}$`)
       .toString()
       .replace(/,/g, '|'),
     'gi',
   );
-  return regExp.test(email);
+	return regExp;
 }
